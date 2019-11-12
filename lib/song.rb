aaa54249bcbf1d19ea68534de55f75276d03fc70
Kevin Downs <kevin.downs04@gmail.com>
2019-11-12 18:35:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,13 @@ class Song
     song
   end
   
+  def self.creat_by_name(name)
+    song = self.new 
+    song.name = name
+    song.save
+    song
+  end
+  
   def self.new_by_name(name)
     song = self.new 
     song.name = name
