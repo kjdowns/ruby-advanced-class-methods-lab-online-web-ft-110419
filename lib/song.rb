797e0c470bad1a1f1223c90c307c1e3a3d84f2a2
Kevin Downs <kevin.downs04@gmail.com>
2019-11-12 19:12:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,6 +23,12 @@ class Song
     song
   end
   
+  def self.create_from_filename(file_name)
+    song = self.create_from_filename(file_name)
+    song.save
+    song
+  end
+  
   def self.new_by_name(name)
     song = self.new 
     song.name = name
